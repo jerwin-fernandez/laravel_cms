2900c9387a578d2163c7159deabd09572313f310
Jerwin Fernandez <jerwinfernandez04@gmail.com>
2020-12-13 12:00:15 +0800

    all setup are good

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,9 +17,11 @@ Route::get('/', function () {
 
 Route::auth();
 
+Route::get('/home', 'HomeController@index');
 
 Route::get('/admin', function(){
     return view('admin.index');
 });
 
-Route::get('/home', 'HomeController@index');
+
+Route::resource('admin/users', 'AdminUsersController');
